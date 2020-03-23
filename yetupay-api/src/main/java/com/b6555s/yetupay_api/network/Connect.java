7e9787cea6fb108a22497a6e3c446e685aca7f69
klausmonga <monga.wakowa@gmail.com>
2020-03-23 17:31:58 +0200

    set ip

--- yetupay-api/src/main/java/com/b6555s/yetupay_api/network/Connect.java
+++ yetupay-api/src/main/java/com/b6555s/yetupay_api/network/Connect.java
@@ -1,5 +1,5 @@
 package com.b6555s.yetupay_api.network;
 
 public class Connect {
-    public  String getDomain(){return "http://localhost/yetupay/Api/process_payment";}
+    public  String getDomain(){return "http://192.168.43.35/yetupay/Api/process_payment";}
 }
