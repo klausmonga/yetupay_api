03e5ee7cdf098e9ccb04904a6654e798acfa18de
klausmonga <monga.wakowa@gmail.com>
2020-03-23 14:58:45 +0200

    Initial commit

---
+++ app/src/main/java/com/b6555s/yetupau_api/MainActivity.java
@@ -0,0 +1,14 @@
+package com.b6555s.yetupau_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
