e717a9beced43bfd487ef5c484f63875bccdf90f
klausmonga <monga.wakowa@gmail.com>
2020-03-25 23:24:46 +0200

    set timeout at 120 000MS

--- yetupay-api/src/main/java/com/b6555s/yetupay_api/network/ProcessPayment.java
+++ yetupay-api/src/main/java/com/b6555s/yetupay_api/network/ProcessPayment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -140,6 +141,10 @@ public class ProcessPayment {
                     }
                 }
             };
+            request.setRetryPolicy(new DefaultRetryPolicy(
+                    120000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
             requestQueue.add(request);
         } catch (JSONException e) {
             e.printStackTrace();
